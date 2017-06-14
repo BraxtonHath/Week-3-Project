f142b9195dc59c5f13a09a8d9dbcfd708c8d0ca6
Braxton Hathaway <braxtonhathaway@Braxtons-MacBook-Air.local>
2017-06-14 13:23:22 -0400

    got displays working js

--- main.js
+++ main.js
@@ -2,17 +2,58 @@
   'use scrict';
 
 
-document.getElementById("display").addEventListener("click", clearDisplay());
-function clearDisplay() {
+
+function clearDisplay(event) {
+  // alert('hweihifhwh');
     var display = document.getElementById('display');
-    display.value = '0';
-    storedNum = '0';
-    calculationFinished = true;
-    operation = operations.none;
+    display.value = '';
 }
 
 
+function numbers(event) {
+    var display = document.getElementById('display');
+    display.value += this.value;
+}
 
+function decimal(event) {
+    var display = document.getElementById('display');
+    display.value += '.';
+}
+
+function operation(event) {
+    var display = document.getElementById('display');
+    display.value += this.value;
+}
+
+function evaluation(event) {
+    var display = document.getElementById('display');
+    alert('yes?');
+    display.value;
+    alert(display.value);
+}
+
+//alert(typeof(numbers));
+//alert(numbers);
+document.getElementById("c").addEventListener("click", clearDisplay);
+
+document.getElementById("zero").addEventListener("click", numbers);
+document.getElementById("one").addEventListener("click", numbers);
+document.getElementById("two").addEventListener("click", numbers);
+document.getElementById("three").addEventListener("click", numbers);
+document.getElementById("four").addEventListener("click", numbers);
+document.getElementById("five").addEventListener("click", numbers);
+document.getElementById("six").addEventListener("click", numbers);
+document.getElementById("seven").addEventListener("click", numbers);
+document.getElementById("eight").addEventListener("click", numbers);
+document.getElementById("nine").addEventListener("click", numbers);
+
+document.getElementById("divide").addEventListener("click", operation);
+document.getElementById("multiply").addEventListener("click", operation);
+document.getElementById("minus").addEventListener("click", operation);
+document.getElementById("add").addEventListener("click", operation);
+
+document.getElementById("decimal").addEventListener("click", decimal);
+document.getElementById("equals").addEventListener("click", evaluation);
 
 
 
